9b40dbaf0227e2cbfcae8e91b1365d18f3433ea3
Vladislav Kulikov <weakvar@gmail.com>
2023-02-19 01:18:25 +0300

    Created a new LayoutAttribute enum

---
+++ Sources/FrameBuilder/LayoutAttribute.swift
@@ -0,0 +1,31 @@
+//
+//  LayoutAttribute.swift
+//  
+//
+//  Created by Vladislav Kulikov on 19.02.2023.
+//
+
+import UIKit
+
+public enum LayoutAttribute {
+    case width(CGFloat)
+    case height(CGFloat)
+    
+    case leading(CGFloat)
+    case trailing(CGFloat)
+    case top(CGFloat)
+    case bottom(CGFloat)
+    
+    case leadingToLeading(ofView: UIView, offset: CGFloat)
+    case leadingToTrailing(ofView: UIView, offset: CGFloat)
+    case trailingToLeading(ofView: UIView, offset: CGFloat)
+    case trailingToTrailing(ofView: UIView, offset: CGFloat)
+    
+    case topToTop(ofView: UIView, offset: CGFloat)
+    case topToBottom(ofView: UIView, offset: CGFloat)
+    case bottomToTop(ofView: UIView, offset: CGFloat)
+    case bottomToBottom(ofView: UIView, offset: CGFloat)
+    
+    case centerXToCenterX(ofView: UIView, offset: CGFloat)
+    case centerYToCenterY(ofView: UIView, offset: CGFloat)
+}
