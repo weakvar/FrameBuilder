f3834273305cac3cc5172c276b34db936f71671d
Vladislav Kulikov <weakvar@gmail.com>
2023-02-19 19:42:05 +0300

    Removed the redundant newlines in the LayoutAttribute

--- Sources/FrameBuilder/LayoutAttribute.swift
+++ Sources/FrameBuilder/LayoutAttribute.swift
@@ -10,22 +10,18 @@ import UIKit
 public enum LayoutAttribute {
     case width(CGFloat)
     case height(CGFloat)
-    
     case leading(CGFloat)
     case trailing(CGFloat)
     case top(CGFloat)
     case bottom(CGFloat)
-    
     case leadingToLeading(ofView: UIView, offset: CGFloat)
     case leadingToTrailing(ofView: UIView, offset: CGFloat)
     case trailingToLeading(ofView: UIView, offset: CGFloat)
     case trailingToTrailing(ofView: UIView, offset: CGFloat)
-    
     case topToTop(ofView: UIView, offset: CGFloat)
     case topToBottom(ofView: UIView, offset: CGFloat)
     case bottomToTop(ofView: UIView, offset: CGFloat)
     case bottomToBottom(ofView: UIView, offset: CGFloat)
-    
     case centerXToCenterX(ofView: UIView, offset: CGFloat)
     case centerYToCenterY(ofView: UIView, offset: CGFloat)
 }
