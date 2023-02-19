517555178bc2024625b47e08bd081f760d560233
Vladislav Kulikov <weakvar@gmail.com>
2023-02-19 21:36:30 +0300

    Prepared an example project for dev

--- Example/FrameBuilderExample/AppDelegate.swift
+++ Example/FrameBuilderExample/AppDelegate.swift
@@ -10,12 +10,16 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
-    }
+    var window: UIWindow?
     
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let messengerViewController = MessengerViewController()
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = messengerViewController
+        window.makeKeyAndVisible()
+        self.window = window
+        
+        return true
     }
     
 }
---
+++ Example/FrameBuilderExample/MessengerViewController.swift
@@ -0,0 +1,10 @@
+//
+//  MessengerViewController.swift
+//  FrameBuilderExample
+//
+//  Created by Vladislav Kulikov on 19.02.2023.
+//
+
+import UIKit
+
+final class MessengerViewController: UIViewController {}
