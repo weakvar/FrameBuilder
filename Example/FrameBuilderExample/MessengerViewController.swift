a07ea8ac8c92b48592e9f88a79b6f3dfd7b59d00
Vladislav Kulikov <weakvar@gmail.com>
2023-02-19 21:48:07 +0300

    Implemented MessengerViewController

--- Example/FrameBuilderExample/MessengerViewController.swift
+++ Example/FrameBuilderExample/MessengerViewController.swift
@@ -7,4 +7,76 @@
 
 import UIKit
 
-final class MessengerViewController: UIViewController {}
+final class MessengerViewController: UIViewController {
+    
+    // MARK: - Views
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.separatorStyle = .none
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+    }()
+    
+    // MARK: - UIViewController Lifecycle
+    
+    override func loadView() {
+        super.loadView()
+        
+        setupViews()
+    }
+    
+    // MARK: - Setup Views
+    
+    func setupViews() {
+        setupNavigationBar()
+        setupTableView()
+    }
+    
+    func setupNavigationBar() {
+        navigationItem.title = "Messenger"
+        navigationItem.largeTitleDisplayMode = .always
+    }
+
+    private func setupTableView() {
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor
+                .constraint(equalTo: view.leadingAnchor),
+            tableView.topAnchor
+                .constraint(equalTo: view.topAnchor),
+            tableView.trailingAnchor
+                .constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor
+                .constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension MessengerViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
+
+// MARK: - UITableViewDelegate
+
+extension MessengerViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
