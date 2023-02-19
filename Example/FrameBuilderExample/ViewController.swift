2af6da4519065001bfe647dde09f3653244efa87
Vladislav Kulikov <weakvar@gmail.com>
2023-02-19 20:49:17 +0300

    Created a new example project

---
+++ Example/FrameBuilderExample/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  FrameBuilderExample
+//
+//  Created by Vladislav Kulikov on 19.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
